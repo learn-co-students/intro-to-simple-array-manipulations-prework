f5499540bc343ad7cae2561a59d7ae1e8db42ad2
Jason  Rosso <jasonrosso@gmail.com>
2017-04-14 17:24:25 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,3 +46,44 @@ end
 #takes in an argument of an array and uses the shift method with an argument
 #of 2 to remove and return the first 2 items from the array
 #decreases the length of the array by 2
+
+#7
+def using_concat(array1,array2)
+  array1.concat(array2)
+end
+
+#8
+def using_insert(array,element)
+  array.insert(4,element)
+end
+#takes in two arguments, an array and a new element to be added to the array.
+#it uses the insert method to add the new element to the 4th index of the array
+#increases the length of the array
+
+
+#9
+def using_uniq(array)
+  array.uniq()
+end
+#takes in an argument of an array and uses the uniq method to remove any
+#duplicate items
+
+
+#10
+def using_flatten(array)
+  array.flatten()
+end
+#takes in an argument of an array that contains other arrays and uses the flat
+#ten method to return an array of strings
+
+#11
+def using_delete(array,string)
+  array.delete(string)
+end
+#takes in two arguments, an array and a string, and uses the delete method to
+#remove any items from the array that are equal to that string
+
+#12
+def using_delete_at(array,integer)
+  array.delete(array[integer])
+end
