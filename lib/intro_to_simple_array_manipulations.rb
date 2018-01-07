dcf4b6b2b8fbcb7a4dee0d0ce48b3a5c024661ae
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2018-01-07 16:50:22 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,11 @@ def using_push(array, string)
   array.push(string)
   return array
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+  return array
+end
+
+def using_pop(array)
+  array.pop
