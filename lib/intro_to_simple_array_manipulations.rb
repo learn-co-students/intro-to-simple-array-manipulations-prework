d838ffadd8e6cf6f1a692366b678ea920c9d6cc2
Bryan Teng <bteng1226@gmail.com>
2018-06-24 01:16:23 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,3 +6,20 @@ def using_unshift(array,string)
   array.unshift(string)
 end
 
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array,array2)
