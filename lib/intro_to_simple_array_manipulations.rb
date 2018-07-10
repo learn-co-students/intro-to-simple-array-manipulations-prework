e766d800100e53d76566c9f2735802520331ae8b
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-10 19:40:25 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,69 @@
+def using_push (arr1, str)
+  arr1.push(str)
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+ return arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  return arr.shift 
+end
+
+def shift_with_args(arr)
+  
+  arr.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  return arr1.concat(arr2)
+end
+
+def using_insert(arr1, arr2)
+  return arr1.insert(4, arr2)
+end
+  
+  
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr1, arr2)
+  return arr1.delete(arr2)
+end
+
+def using_delete_at(arr, num)
+  return arr.delete_at(num)
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
