d1875330cdb8d90f57de0d2e15ef5df7c1d91b1d
paristuzun <paristuzun@gmail.com>
2018-10-19 01:47:05 -0400

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,75 @@
+#1
+def using_push(array, string)
+	array.push(string)
+end
+
+#2
+def using_unshift(array, string)
+	array.unshift(string)
+end	
+
+#3
+def using_pop(array)
+	array.pop
+end	
+
+#4
+def pop_with_args(characters_in_got)
+	characters_in_got.pop(2)
+end
+
+#5
+def using_shift(characters_in_got)
+	characters_in_got.shift
+end
+
+#6
+def shift_with_args(characters_in_got)
+	characters_in_got.shift(2)
+end
+
+#7
+def using_concat(favorite_things, new_favorite)
+	favorite_things.concat(new_favorite)
+end
+
+#8
+def using_insert(favorite_things, new_favorite)
+	favorite_things.insert(4, new_favorite)
+end
+
+#9
+def using_uniq(favorite_things)
+	favorite_things.uniq
+end
+
+#10
+def using_flatten(array)
+	array.flatten
+end
+
+#11
+def using_delete(array, string)
+	array.delete(string)
+end
+
+#11
+def using_delete_at(array, integer)
+	array.delete_at(integer)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
