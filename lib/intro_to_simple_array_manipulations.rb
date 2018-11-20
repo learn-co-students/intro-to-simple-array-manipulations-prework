b669682892ab2fba4485905a27f49a548ed7b61c
cyn7117 <cyn7117@gmail.com>
2018-11-20 21:44:04 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,69 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(arr, st)
+  arr.unshift(st)
+  
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+
+def using_shift(arr)
+  arr.shift
+end
+
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+  
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, el)
+  arr.insert(4, el)
+end
+
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+
+def using_delete(arr, st)
+  arr.delete(st)
+end
+
+
+
+def using_delete_at(arr, n)
+  arr.delete_at(n)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
