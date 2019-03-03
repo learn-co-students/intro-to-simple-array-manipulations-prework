e82968a3db90812b6ddb1c6ad5e711fa219134cf
Lisa Scott <lisarenesoctt@gmail.com>
2019-03-02 19:20:30 -0500

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,48 +1,37 @@
-def using_push(countries_in_western_africa, next_country )
-  countries_in_western_africa.push(next_country)
-  #end
+def using_push(array, string)
+  array << string
 end
-def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
-  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
-#  end
+def using_unshift(array, string)
+array.unshift(string)
 end
-
-def using_pop(deleted_string)
-  deleted_string.pop
+def using_pop(array)
+  x = array.pop
+  x
 end
-
-def pop_with_args(chars_arya_killed)
-chars_arya_killed.pop(2)
+def pop_with_args(array)
+  array.pop(2)
 end
-
-def using_shift(my_favorite_cities)
-  my_favorite_cities.shift
+def using_shift(array)
+  array.shift
 end
-
-def shift_with_args(brands_removed)
-  brands_removed.shift(2)
+def shift_with_args(array)
+  array.shift(2)
 end
-
-def using_concat(my_favorite_things, more_favs)
-  my_favorite_things.concat(more_favs)
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
 end
-
-def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
-  list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
+def using_insert(array, element)
+  array.insert(4, element)
 end
-
-def using_uniq(captain_planet_and_the_planeteers)
-captain_planet_and_the_planeteers.uniq
+def using_uniq(array)
+  array.uniq
 end
-
-def using_flatten(private_colleges_in_newyork)
-  private_colleges_in_newyork.flatten
+def using_flatten(array)
+  array.flatten
 end
-
-def using_delete(instructors, no_offense_steven)
-  instructors.delete(no_offense_steven)
+def using_delete(array, string)
+  array.delete(string)
 end
-
-def using_delete_at(famous_robots, integer)
-  famous_robots.delete_at(2)
+def using_delete_at(array, num)
+  array.delete_at(2)
 end
